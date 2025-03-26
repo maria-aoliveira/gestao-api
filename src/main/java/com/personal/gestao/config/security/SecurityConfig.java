45b41b2b8ec730a02a104b7c0a708a27d2423bdb
Maria Eduarda <maria.aoliveira@al.infnet.edu.br>
2025-03-26 00:33:29 -0300

    reestruturação para configurações e acrescenta cors

--- src/main/java/com/personal/gestao/config/security/SecurityConfig.java
+++ src/main/java/com/personal/gestao/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.personal.gestao.config;
+package com.personal.gestao.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
